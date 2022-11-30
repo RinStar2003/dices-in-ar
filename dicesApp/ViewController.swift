57c07721f71e17c09538f1e349265bb3f617e53d
RinStar2003 <mituha133@gmail.com>
2022-11-30 16:19:54 +0200

    - Now only one table can be spawned - Testing adding dice

--- dicesApp/ViewController.swift
+++ dicesApp/ViewController.swift
@@ -12,6 +12,10 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    var isTableAdded = false
+    
+    var diceArray = [SCNNode]()
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,27 +51,54 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("Tapped")
 
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResults = results.first {
+                
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    
+                    diceNode.position = SCNVector3(
+                        x: hitResults.worldTransform.columns.3.x,
+                        y: hitResults.worldTransform.columns.3.y,
+                        z: hitResults.worldTransform.columns.3.z)
+                    
+                    diceArray.append(diceNode)
+                    
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                }
+            }
+        }
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         print("plane detected")
 
-        let planeAnchor = anchor
-       // let planeNode = SCNNode()
-        let x = planeAnchor.transform.columns.3.x
-        let y = planeAnchor.transform.columns.3.y
-        let z = planeAnchor.transform.columns.3.z
-       //planeNode!.position = SCNVector3(x: x, y: y, z: z)
-    
-       let idleScene = SCNScene(named: "art.scnassets/Table.scn")!
-             let node = SCNNode()
-             for child in idleScene.rootNode.childNodes{
-                 node.addChildNode(child)
-             }
-             node.position = SCNVector3(x,y,z)
-             node.scale = SCNVector3(0.001,0.001,0.001)
-             sceneView.scene.rootNode.addChildNode(node)
-        
+        if isTableAdded == false {
+            let planeAnchor = anchor
+            // let planeNode = SCNNode()
+            let x = planeAnchor.transform.columns.3.x
+            let y = planeAnchor.transform.columns.3.y
+            let z = planeAnchor.transform.columns.3.z
+            //planeNode!.position = SCNVector3(x: x, y: y, z: z)
+            
+            let idleScene = SCNScene(named: "art.scnassets/Table.scn")!
+            let node = SCNNode()
+            for child in idleScene.rootNode.childNodes{
+                node.addChildNode(child)
+            }
+            node.position = SCNVector3(x,y,z)
+            node.scale = SCNVector3(0.001,0.001,0.001)
+            sceneView.scene.rootNode.addChildNode(node)
+            
+            isTableAdded = true
+        }
         print("Model placed")
     }
     
